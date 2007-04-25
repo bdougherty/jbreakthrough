259e533148d067746437b738e984ef65e7266bdb
bdougherty <bdougherty@6e6cebfc-d02e-0410-8db7-05aa5890a72b>
2007-04-25 16:22:06 +0000

    Added listener to report coordinates

--- trunk/Breakthrough.java
+++ trunk/Breakthrough.java
@@ -18,14 +18,7 @@ public class Breakthrough extends JFrame {
 	
 	// Attributes
 	JLabel infoLabel;
-	JButton [][] buttons = { { new JButton("0,0"), new JButton("1,0"), new JButton("2,0"), new JButton("3,0"), new JButton("4,0"), new JButton("5,0"), new JButton("6,0"), new JButton("7,0") },
-							 { new JButton("0,1"), new JButton("1,1"), new JButton("2,1"), new JButton("3,1"), new JButton("4,1"), new JButton("5,1"), new JButton("6,1"), new JButton("7,1") },
-							 { new JButton("0,2"), new JButton("1,2"), new JButton("2,2"), new JButton("3,2"), new JButton("4,2"), new JButton("5,2"), new JButton("6,2"), new JButton("7,2") },
-							 { new JButton("0,3"), new JButton("1,3"), new JButton("2,3"), new JButton("3,3"), new JButton("4,3"), new JButton("5,3"), new JButton("6,3"), new JButton("7,3") },
-							 { new JButton("0,4"), new JButton("1,4"), new JButton("2,4"), new JButton("3,4"), new JButton("4,4"), new JButton("5,4"), new JButton("6,4"), new JButton("7,4") },
-							 { new JButton("0,5"), new JButton("1,5"), new JButton("2,5"), new JButton("3,5"), new JButton("4,5"), new JButton("5,5"), new JButton("6,5"), new JButton("7,5") },
-							 { new JButton("0,6"), new JButton("1,6"), new JButton("2,6"), new JButton("3,6"), new JButton("4,6"), new JButton("5,6"), new JButton("6,6"), new JButton("7,6") },
-							 { new JButton("0,7"), new JButton("1,7"), new JButton("2,7"), new JButton("3,7"), new JButton("4,7"), new JButton("5,7"), new JButton("6,7"), new JButton("7,7") } };
+	JButton [][] button = new JButton[8][8];
 	int [][] pieces = new int[8][8];
 	
 	/**
@@ -44,20 +37,21 @@ public class Breakthrough extends JFrame {
 		infoPanel.add(infoLabel = new JLabel("Woohoo!"));
 		
 		// Buttons
-		JPanel buttonPanel = new JPanel(new GridLayout(0,8));
-		for (int i = 0; i < buttons.length; i++) {
-			for (int j = 0; j < buttons[i].length; j++) {
-				// buttons[i][j].setSize(25,25);
+		JPanel buttonPanel = new JPanel(new GridLayout(8,8));
+		for (int i = 0; i < button.length; i++) {
+			for (int j = 0; j < button[i].length; j++) {
+				button[i][j] = new JButton();
 				if (j == 0 || j == 1) {
-					buttons[i][j].setText("");
-					buttons[i][j].setIcon(new ImageIcon("team1.jpg"));
+					button[i][j].setText("");
+					button[i][j].setIcon(new ImageIcon("team1.jpg"));
 				}
 				else if (j == 6 || j == 7) {
-					buttons[i][j].setText("");
-					buttons[i][j].setIcon(new ImageIcon("team2.jpg"));
+					button[i][j].setText("");
+					button[i][j].setIcon(new ImageIcon("team2.jpg"));
 				}
 				
-				buttonPanel.add(buttons[i][j]);
+				buttonPanel.add(button[i][j]);
+				button[i][j].addActionListener(new ButtonListener(i, j));
 			}
 		}
 		
@@ -78,6 +72,20 @@ public class Breakthrough extends JFrame {
 	 */
 	public static void main(String[] args) {
 		new Breakthrough();
+	}
+	
+	/**
+	 * DEBUG BUTTON LISTENER
+	 */
+	class ButtonListener implements ActionListener {
+		int x, y;
+		public ButtonListener(int i, int j) {
+			this.y = i;
+			this.x = j;
+		}
+		public void actionPerformed(ActionEvent ae) {
+			System.out.println("Coordinates: " + x + "," + y);
+		}
 	}
 	
 }
