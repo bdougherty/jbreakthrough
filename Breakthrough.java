101f69fd2d853041ba37a7f8559fcfff3379fb76
bdougherty <bdougherty@6e6cebfc-d02e-0410-8db7-05aa5890a72b>
2007-04-26 01:28:36 +0000

    Combined GUIs into main class

--- Breakthrough.java
+++ Breakthrough.java
@@ -1,10 +1,9 @@
 /**
- * BREAKTHROUGH game
+ * BREAKTHROUGH game client
  * 
  * @version .1
  * 
  * @author Brad Dougherty
- * @author Kevin Harris
  * 
  */
 
@@ -14,28 +13,136 @@ import javax.swing.*;
 import java.net.*;
 import java.io.*;
 
-public class Breakthrough extends JFrame {
+public class Breakthrough {
 	
 	// Attributes
-	JLabel infoLabel;
-	JButton [][] button = new JButton[8][8];
-	int [][] pieces = new int[8][8];
-	int team = 1; // TEMP FOR DEBUGGING
+	private JTextField address, name;
+	private JButton start;
+	private JLabel infoLabel, status;
+	private JButton [][] button = new JButton[8][8];
+	private String myName, myOpponent;
+	private int team;
+	private JFrame configFrame, gameFrame;
+	
+	/**
+	 * Set the game options and show the main GUI
+	 * WILL ADD SOCKET IN THE FUTURE
+	 */
+	private void initializeGame(String myName, String myOpponent, int team) {
+		this.myName = myName;
+		this.myOpponent = myOpponent;
+		this.team = team;
+		createAndShowGUI();
+	}
 	
 	/**
 	 * Default constructor
 	 * Creates the GUI
 	 */
-	public Breakthrough() {
+	public void createAndShowConfig() {
+		
+		configFrame = new JFrame();
 		
 		// Set frame options
-		setTitle("Breakthrough");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(false);
+		configFrame.setTitle("Breakthrough");
+		configFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		configFrame.setResizable(false);
+		configFrame.setBackground(Color.WHITE);
+		
+		// Image panel
+		JPanel imagePanel = new JPanel();
+		imagePanel.add(new JLabel(new ImageIcon("title.jpg")));
+		
+		// Fields and connect button
+		JPanel optionsPanel = new JPanel(new GridLayout(0, 2, 10, 10));
+		optionsPanel.add(new JLabel("Server address:"));
+		optionsPanel.add(address = new JTextField(10));
+		optionsPanel.add(new JLabel("Your name:"));
+		optionsPanel.add(name = new JTextField(10));
+		JPanel fieldsPanel = new JPanel();
+		fieldsPanel.add(optionsPanel);
+		JPanel connectPanel = new JPanel();
+		connectPanel.add(start = new JButton("Connect!"));
+		JPanel center = new JPanel(new GridLayout(0,1));
+		center.add(fieldsPanel);
+		center.add(connectPanel);
+		
+		// Status panel
+		JPanel statusPanel = new JPanel();
+		statusPanel.setBackground(Color.LIGHT_GRAY);
+		statusPanel.add(status = new JLabel("Status: Waiting for information"));
+		
+		// Add the panels to the frame
+		configFrame.add(imagePanel, BorderLayout.NORTH);
+		configFrame.add(center);
+		configFrame.add(statusPanel, BorderLayout.SOUTH);
+		
+		// Add listeners
+		start.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae) {
+				
+				// Change GUI
+				address.setEnabled(false);
+				name.setEnabled(false);
+				start.setEnabled(false);
+				status.setText("Status: Connecting to server");
+				
+				try {
+					Thread.sleep(100);
+				}
+				catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+				}
+				
+				// Connect to the server
+				// Socket sock = new Socket(address.getText(), 4567);
+
+				// Sends name to server
+				// out.write(name.getText());
+				// out.newLine();
+				// out.flush();
+				status.setText("Status: Waiting for opponent");
+				
+				try {
+					Thread.sleep(100);
+				}
+				catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+				}
+				
+				// Recieve back opponents information
+				// String response = in.readLine();
+				int team = 1; // 1 for now... = Integer.parseInt(response.substring(0,1));
+				String opponentName = "Travis"; // = response.substring(2,response.length);
+				status.setText("Status: Setting up game");
+				
+				initializeGame(name.getText(), opponentName, team);
+				configFrame.setVisible(false);
+				
+			}
+		});
+		
+		// Pack and show
+		configFrame.pack();
+		configFrame.setLocationRelativeTo(null); // Center on screen
+		configFrame.setVisible(true);
+		
+	}
+	
+	/**
+	 * Creates the GUI
+	 */
+	public void createAndShowGUI() {
+		
+		gameFrame = new JFrame();
+		
+		// Set frame options
+		gameFrame.setTitle("Breakthrough");
+		gameFrame.setResizable(false);
 		
 		// Top information panel
 		JPanel infoPanel = new JPanel();
-		infoPanel.add(infoLabel = new JLabel("Woohoo!"));
+		infoPanel.add(infoLabel = new JLabel("Woohoo! Welcome to Breakthrough, "+myName+".\nYou are playing against "+myOpponent));
 		
 		// Reverse the board if you are team 2
 		JPanel buttonPanel = new JPanel(new GridLayout(8,0));
@@ -79,22 +186,30 @@ public class Breakthrough extends JFrame {
 		}
 		
 		// Add the panels to the frame
-		add(infoPanel, BorderLayout.NORTH);
-		add(buttonPanel);
+		gameFrame.add(infoPanel, BorderLayout.NORTH);
+		gameFrame.add(buttonPanel);
 		
 		// Pack and show
-		pack();
-		setLocationRelativeTo(null); // Center on screen
-		setVisible(true);
+		gameFrame.pack();
+		gameFrame.setLocationRelativeTo(null); // Center on screen
+		gameFrame.setVisible(true);
 		
 	}
 	
 	/**
-	 * Main method
+	 * Main method - Starts the Configuration GUI
 	 * @param args command-line arguments
 	 */
 	public static void main(String[] args) {
 		new Breakthrough();
+	}
+	
+	/**
+	 * Default Constructor
+	 * Shows the Configuration frame
+	 */
+	public Breakthrough() {
+		createAndShowConfig();
 	}
 	
 }
