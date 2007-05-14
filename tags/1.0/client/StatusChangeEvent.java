437379a005695dcd3156b5a41992d4bd2e062b6c
BDougherty <BDougherty@6e6cebfc-d02e-0410-8db7-05aa5890a72b>
2007-05-14 05:37:03 +0000

    Tagging the 1.0 release

---
+++ tags/1.0/client/StatusChangeEvent.java
@@ -0,0 +1,55 @@
+/**
+ * Breakthrough Game<br />
+ * RIT 4002-219 Final Project<br />
+ * Date: May 14, 2007
+ * @author Brad Dougherty, Kevin Harris
+ * @version 1.0
+ * Breakthrough Client Connection Event
+ */
+
+import java.awt.Color;
+
+public class StatusChangeEvent {
+	private Color color;
+	private String status;
+	
+	static final Color ERROR_COLOR = new Color(190,2,2);
+	static final Color ALERT_COLOR = new Color(0,161,81);
+	static final Color CONFIG_COLOR = Color.LIGHT_GRAY;
+	
+	/**
+	 * Constructor
+	 * @param status The status text
+	 */
+	public StatusChangeEvent(String status) {
+		this.status = status;
+		this.color = null;
+	}
+	
+	/**
+	 * Constructor
+	 * @param status The status text
+	 * @param color The color
+	 */
+	public StatusChangeEvent(String status, Color color) {
+		this.status = status;
+		this.color = color;
+	}
+	
+	/**
+	 * Get Color
+	 * @return color The color
+	 */
+	public Color getColor() {
+		return color;
+	}
+	
+	/**
+	 * Get Status
+	 * @return status The status
+	 */
+	public String getStatus() {
+		return status;
+	}
+	
+}
